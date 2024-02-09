c9a8437f3de65ce8d822a973f8765ce38bde6adb
euouae682 <jasonuzhao682@gmail.com>
2024-02-08 18:19:11 -0800

    Removed Head tag from Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <main>
-      <Head>
-        <title>Net Impact - Home</title>
-      </Head>
       <h1>Home</h1>
     </main>
   );
