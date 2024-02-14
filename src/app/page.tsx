ddbabf553bec0b21ce63e7f8b03651867be9563c
euouae682 <jasonuzhao682@gmail.com>
2024-02-13 19:30:34 -0800

    Made positioning of text/buttons absolute

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,10 +8,14 @@ export default function Home() {
     <main>
       <NavBar />
       <header className="m-auto">
-        <div className='m-auto my-5 h-[512px] w-[1193px] bg-center bg-cover rounded-3xl flex flex-col-reverse' style={{
+        <div className='relative m-auto my-5 h-[512px] w-[1193px] bg-center bg-cover rounded-3xl' style={{
           backgroundImage: `url(${bg.src})`,
         }}>
-          <div className="flex gap-[23px] justify-center py-8">
+          <div className="absolute top-[184px] left-[57px] w-1/2">
+            <h1 className="text-[68px] uppercase text-white">Net Impact UW</h1>
+            <h2 className="text-[26px] leading-[30px] text-white">We are a sustainable business club,<br/>open for all undergraduate students</h2>
+          </div> 
+          <div className="absolute bottom-0 w-full flex gap-[23px] justify-center py-8">
             <div className="bg-neutral-400 w-[883px] h-[38px] rounded-full text-center leading-8 text-white">
               <p>We aim to inspire environmental action and increase awareness through <strong>Education</strong>, <strong>Activism</strong>, & <strong>Career Development</strong>.</p>
             </div>
@@ -19,10 +23,6 @@ export default function Home() {
               JOIN US
             </button>
           </div>
-          <div className="w-1/2 pt-[184px] pb-[80px] pl-[57px]">
-            <h1 className="text-[68px] uppercase text-white">Net Impact UW</h1>
-            <h2 className="text-[26px] leading-[30px] text-white">We are a sustainable business club,<br/>open for all undergraduate students</h2>
-          </div> 
         </div>
       </header>
       <div>
