fbbc61914994f05941a3fea251aaf025f4b407d2
bellchen5@gmail.com <bellchen5@gmail.com>
2024-02-13 17:46:51 -0800

    made navbbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-
+import NavBar from "./components/Navbar";
 export default function Home() {
   return (
     <main>
       <h1>Test Test</h1>
       <p className="" >Hello World!</p>
+      <NavBar/>
     </main>
   );
 }
