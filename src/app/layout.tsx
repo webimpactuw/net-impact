b7f67636575b042584c98066f30135dbffd4ea6a
euouae682 <jasonuzhao682@gmail.com>
2024-02-13 17:56:13 -0800

    replaced default font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Barlow } from 'next/font/google';
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const barlow = Barlow({
+  weight: ['600', '900'],
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: {
@@ -20,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={barlow.className}>{children}</body>
     </html>
   );
 }
