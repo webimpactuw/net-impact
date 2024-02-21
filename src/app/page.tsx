5261b0c7965d36a9d3484f726d56a8a48a9d16bf
euouae682 <jasonuzhao682@gmail.com>
2024-02-20 18:05:51 -0800

    added slight responsivity to width measurements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,23 @@ import NavBar from "./components/NavBar";
 
 import bg from '../../public/headerimg.jpg';
 
-// TODO: replace manual widths/heights
 export default function Home() {
   return (
     <main>
       <NavBar />
       <header className="m-auto">
-        <div className='relative m-auto my-5 h-[512px] w-[1193px] bg-center bg-cover rounded-3xl' style={{
-          backgroundImage: `url(${bg.src})`,
+        <div className='relative m-auto my-5 h-[512px] w-11/12 bg-center bg-cover rounded-3xl bg-opacity-30' style={{
+          backgroundImage: `url(${bg.src})`
         }}>
-          <div className="absolute top-[184px] left-[57px] w-1/2">
-            <h1 className="text-[68px] uppercase text-white">Net Impact UW</h1>
-            <h2 className="text-[26px] leading-[30px] text-white">We are a sustainable business club,<br/>open for all undergraduate students</h2>
+          <div className="absolute top-[36%] left-14 w-1/2">
+            <h1 className="text-6xl uppercase text-white font-[900]">Net Impact UW</h1>
+            <h2 className="text-2xl text-white">We are a sustainable business club,<br/>open for all undergraduate students</h2>
           </div> 
-          <div className="absolute bottom-0 w-full flex gap-[23px] justify-center py-8">
-            <div className="bg-neutral-400 w-[883px] h-[38px] rounded-full text-center leading-8 text-white">
+          <div className="absolute bottom-0 w-full flex gap-6 justify-center py-8">
+            <div className="bg-neutral-400 w-3/4 h-10 rounded-full text-center leading-8 text-white font-[400]">
               <p>We aim to inspire environmental action and increase awareness through <strong>Education</strong>, <strong>Activism</strong>, & <strong>Career Development</strong>.</p>
             </div>
-            <button className="bg-white w-[110px] h-[38px] rounded-full text-center">
+            <button className="bg-white w-1/12 h-10 rounded-full text-center">
               JOIN US
             </button>
           </div>
