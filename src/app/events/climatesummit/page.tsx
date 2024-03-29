49667a180bcc8c7098c26d67726c6b6c31e5a7ec
euouae682 <jasonuzhao682@gmail.com>
2024-03-28 18:24:13 -0700

    Added extra routes

---
+++ src/app/events/climatesummit/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Climate Solutions Summit',
+}
+
+export default function Summit() {
+  return (
+    <main>
+      <h1>Climate Solutions Summit</h1>
+    </main>
+  );
+}
---
+++ src/app/events/fallpanel/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Fall Panel',
+}
+
+export default function FallPanel() {
+  return (
+    <main>
+      <h1>Fall Panel</h1>
+    </main>
+  );
+}
