9a454f6bc99fe1189eb7a8590a95cd92d28f7518
euouae682 <jasonuzhao682@gmail.com>
2024-04-25 18:03:38 -0700

    Put navbar and footer into layout.tsx

--- src/app/about/ourteam/page.tsx
+++ src/app/about/ourteam/page.tsx
@@ -1,6 +1,4 @@
 import { Metadata } from 'next'
-import NavBar from '@/app/components/NavBar';
-import Footer from '@/app/components/Footer';
 import TeamPage from '@/app/components/TeamPage';
  
 export const metadata: Metadata = {
@@ -10,7 +8,6 @@ export const metadata: Metadata = {
 export default function OurTeam() {
   return (
     <div className="bg-slate-100">
-      <NavBar />
       <div className="h-[110px]">
         <svg xmlns="http://www.w3.org/2000/svg" width="1036" height="1160" viewBox="0 0 1036 1160" fill="none">
           <path d="M123.474 -410.903C120.323 -341.188 109.968 -263.884 130.086 -195.791C158.109 -100.944 33.3817 -253.702 75.4997 -164.903C108.514 -95.2973 157 68.0967 402.062 240.164C511.533 310.846 622.921 375.335 738.836 434.778C817.272 475.002 901.347 527.863 964.086 590.602C983.944 610.46 998.19 637.329 1013.46 660.69C1041.42 703.476 1065.93 748.362 1087.51 794.694C1117.22 858.477 1131.15 921.933 1131.15 991.954C1131.15 1025.03 1136.46 1061.78 1123.22 1092.68" stroke="white" stroke-width="133.326" stroke-linecap="round"/>
@@ -33,7 +30,6 @@ export default function OurTeam() {
           <TeamPage img="gnavarro.png" name="Gabriel Navarro" role="Director of Outreach" year="Senior" major="Marketing, Entrepreneurship" minor="" statement="placeholder" linkedin="test" twit="test" instagram="test" />
         </div>
       </main>
-      <Footer/>
     </div>
   );
 }
