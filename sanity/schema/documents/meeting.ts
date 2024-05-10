7f8a5f0595a8a27f8594586f1f4e52e15afa6132
euouae682 <jasonuzhao682@gmail.com>
2024-05-09 18:11:40 -0700

    implemented sanify functionality in sponsors, meeting, teammembers

---
+++ sanity/schema/documents/meeting.ts
@@ -0,0 +1,27 @@
+export const meetingType = {
+    name: 'meeting',
+    title: 'MeetingInfo',
+    type: 'document',
+    fields: [
+        {
+            name: 'type',
+            type: 'string',
+        },
+        {
+            name: 'days',
+            type: 'string',
+        },
+        {
+            name: 'time',
+            type: 'string',
+        },
+        {
+            name: 'location',
+            type: 'string',
+        },
+        {
+            name: 'description',
+            type: 'string'
+        }
+    ]
+}
