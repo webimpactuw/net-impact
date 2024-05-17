4e5f632f2e3d953ac4b0519611044ba76baddc9a
euouae682 <jasonuzhao682@gmail.com>
2024-05-16 18:02:50 -0700

    added team page img

--- src/app/about/ourteam/page.tsx
+++ src/app/about/ourteam/page.tsx
@@ -5,6 +5,7 @@ import { client, sanityFetch } from "@/sanity/client";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import imageUrlBuilder from "@sanity/image-url";
 import { BiPhotoAlbum } from 'react-icons/bi';
+import Image from 'next/image';
 
 const TEAM_QUERY = `*[_type == "team"]`;
 
@@ -29,10 +30,14 @@ export default async function OurTeam() {
         <svg xmlns="http://www.w3.org/2000/svg" width="1036" height="1160" viewBox="0 0 1036 1160" fill="none">
           <path d="M123.474 -410.903C120.323 -341.188 109.968 -263.884 130.086 -195.791C158.109 -100.944 33.3817 -253.702 75.4997 -164.903C108.514 -95.2973 157 68.0967 402.062 240.164C511.533 310.846 622.921 375.335 738.836 434.778C817.272 475.002 901.347 527.863 964.086 590.602C983.944 610.46 998.19 637.329 1013.46 660.69C1041.42 703.476 1065.93 748.362 1087.51 794.694C1117.22 858.477 1131.15 921.933 1131.15 991.954C1131.15 1025.03 1136.46 1061.78 1123.22 1092.68" stroke="white" stroke-width="133.326" stroke-linecap="round"/>
         </svg>
-      </div>
+      </div> 
       <main className="px-16 py-28 flex flex-col gap-20 text-[#11122D] bg-white z-10">
         <p className="text-[#2F8097] text-[20px] font-medium leading-[150%]">Our Team</p>
         <h1 className="text-[48px] font-medium leading-[120%]">Introduce the team</h1>
+        <figure className="m-auto relative overflow-hidden w-[95%] h-[400px] lg:h-[700px] bg-[url('/teamimg.png')] bg-cover bg-center rounded-3xl z-10">
+          <Image alt="teamimg" src="/team/teamimg.png" layout="fill" objectFit="cover" />
+        </figure>
+
         <div className="flex justify-between flex-wrap">
           {
             members.map((member: { photo: any; name: any; role: any; year: any; major: any; minor: any; description: any; linkedin: any; twitter: any; instagram: any; }) => {
